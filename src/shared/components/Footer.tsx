430ea32855814fe5d595435b3b01341c0322197d
Starck27 <adinataekwan27@gmail.com>
2026-02-28 01:13:51 +0700

    fix: fix ui responsivity in small screen

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="flex flex-col justify-center items-center w-full">
-      <div className="px-6 py-4 w-full max-w-2xl">
+      <div className="px-6 py-4 w-full max-w-2xl text-center">
         <p>&copy; 2026 Splitted. All Rights Reserved.</p>
       </div>
     </footer>
